9b8061ecd293aa11d00a2acdd6c79a2aae0e0a06
milanmaniya <milanmaniya2035@gmail.com>
2023-09-16 23:22:01 +0530

    done

--- lib/firebase_api/student_firebase_api/student_firebase_api.dart
+++ lib/firebase_api/student_firebase_api/student_firebase_api.dart
@@ -2,9 +2,15 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:grade_up/common_model/common_student_post_model.dart';
 
 class StudentFirebaseApi {
+
+
+
   static DatabaseReference db = FirebaseDatabase.instance.ref('student');
 
   static Future<void> setStudentData() async {
+
+  
+
     String key = db.push().key!;
 
     for (int i = 0; i < studentPostList.length; i++) {
