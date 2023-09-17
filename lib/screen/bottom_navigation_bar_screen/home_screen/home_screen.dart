be242ff0d5906403a24323c2fd44bf5e7eb5c898
milanmaniya <milanmaniya2035@gmail.com>
2023-09-17 23:40:28 +0530

    done

--- lib/screen/bottom_navigation_bar_screen/home_screen/home_screen.dart
+++ lib/screen/bottom_navigation_bar_screen/home_screen/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:giffy_dialog/giffy_dialog.dart';
 import 'package:grade_up/common_widget/common_app_bar.dart';
 import 'package:grade_up/screen/bottom_navigation_bar_screen/home_screen/tab_bar_screen/online_courses_screen/online_course_screen.dart';
 import 'package:grade_up/screen/bottom_navigation_bar_screen/home_screen/tab_bar_screen/tuition_screen/tuition_screen.dart';
@@ -32,36 +31,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
           prefixIcon: Icons.menu_open_rounded,
           fun1: () {},
           fun2: () {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return GiffyDialog.image(
-                  Image.network(
-                    "https://raw.githubusercontent.com/Shashank02051997/FancyGifDialog-Android/master/GIF's/gif14.gif",
-                    height: 200,
-                    fit: BoxFit.cover,
-                  ),
-                  title: const Text(
-                    'Image Animation',
-                    textAlign: TextAlign.center,
-                  ),
-                  content: const Text(
-                    'This is a image animation dialog box. This library helps you easily create fancy giffy dialog.',
-                    textAlign: TextAlign.center,
-                  ),
-                  actions: [
-                    TextButton(
-                      onPressed: () => Navigator.pop(context, 'CANCEL'),
-                      child: const Text('CANCEL'),
-                    ),
-                    TextButton(
-                      onPressed: () => Navigator.pop(context, 'OK'),
-                      child: const Text('OK'),
-                    ),
-                  ],
-                );
-              },
-            );
+            
           },
           tagName: 'Menu',
           actionFirstIcon: Icons.search,
