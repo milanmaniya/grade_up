58c429ad9b3b8f9cffe9a3b9b0afbe9883bdd3b1
milanmaniya <milanmaniya2035@gmail.com>
2024-03-02 16:30:55 +0530

    done

--- lib/google e books api/screens/basic.dart
+++ lib/google e books api/screens/basic.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
-
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 class BasicBookScreen extends StatefulWidget {
   const BasicBookScreen({super.key});
@@ -13,8 +14,8 @@ class BasicBookScreen extends StatefulWidget {
 class _BasicBookScreenState extends State<BasicBookScreen> {
   late Future<List<dynamic>> books;
 
-  Future<List> fetchBooks() async {
-    const url = "https://www.googleapis.com/books/v1/volumes?q=java";
+  Future<List> fetchBooks(String key) async {
+    final url = "https://www.googleapis.com/books/v1/volumes?q=$key";
 
     final uri = Uri.parse(url);
 
@@ -24,9 +25,6 @@ class _BasicBookScreenState extends State<BasicBookScreen> {
       final result = jsonDecode(response.body) as Map;
 
       final data = result['items'] as List;
-
-      // log(data.toString());
-
       return data;
     } else {
       return [];
@@ -35,96 +33,111 @@ class _BasicBookScreenState extends State<BasicBookScreen> {
 
   final txtSearch = TextEditingController();
 
+  Future<void> urlLauncher(String url) async {
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {}
+  }
+
   @override
   void initState() {
-    books = fetchBooks();
+    books = fetchBooks(txtSearch.text);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Library'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 10,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Library'),
         ),
-        child: Column(
-          children: [
-            TextField(
-              controller: txtSearch,
-              onChanged: (value) {
-                setState(() {
-                  txtSearch.text = value;
-                });
-              },
-              decoration: InputDecoration(
-                labelText: 'Search',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: BorderSide(color: Colors.grey.shade300),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+            vertical: 10,
+          ),
+          child: Column(
+            children: [
+              TextField(
+                controller: txtSearch,
+                onChanged: (value) {
+                  setState(() {
+                    txtSearch.text = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Search',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: RefreshIndicator(
-                onRefresh: fetchBooks,
+              Expanded(
                 child: FutureBuilder(
-                  future: fetchBooks(),
+                  future: fetchBooks(txtSearch.text),
                   builder: (context, snapshot) {
                     final data = [];
 
                     if (snapshot.hasData) {
                       for (var element in snapshot.data!) {
-                        // log(element['volumeInfo'].toString());
                         data.add(element['volumeInfo']);
                       }
                     }
 
-                    return ListView.builder(
-                      itemCount: data.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(
-                            data[index]['title'],
-                            maxLines: 1,
-                            style: const TextStyle(
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          subtitle: Text(
-                            data[index]!['publisher'],
-                            maxLines: 1,
-                            style: const TextStyle(
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          trailing: Text(data[index]['publishedDate']),
-                          leading: Container(
-                            height: 100,
-                            width: 100,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                  data[index]['imageLinks']['thumbnail'],
-                                ),
-                                fit: BoxFit.cover,
+                    if (data.isNotEmpty) {
+                      return ListView.builder(
+                        itemCount: data.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            onTap: () {
+                              urlLauncher(data[index]['previewLink']);
+                            },
+                            title: Text(
+                              data[index]['title'],
+                              maxLines: 1,
+                              style: const TextStyle(
+                                overflow: TextOverflow.ellipsis,
                               ),
                             ),
-                          ),
-                        );
-                      },
-                    );
+                            subtitle: Text(
+                              data[index]!['publisher'].toString(),
+                              maxLines: 1,
+                              style: const TextStyle(
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            trailing:
+                                Text(data[index]['publishedDate'].toString()),
+                            leading: SizedBox(
+                              height: 100,
+                              width: 100,
+                              child: data[index]['imageLinks']['thumbnail'] !=
+                                      null
+                                  ? Image.network(
+                                      data[index]['imageLinks']['thumbnail'],
+                                      height: 120,
+                                      width: 100,
+                                      fit: BoxFit.cover,
+                                    )
+                                  : const SizedBox(),
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return const Center(
+                        child: Text('Data not found'),
+                      );
+                    }
                   },
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
